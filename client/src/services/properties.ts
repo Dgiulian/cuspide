9bc5415869edd33937757813733f5d04b437dd70
Diego Giuliani <giuliani.diego@gmail.com>
2024-09-26 10:23:22 -0300

    Working on property details

--- client/src/services/properties.ts
+++ client/src/services/properties.ts
@@ -1,4 +1,52 @@
 import { type BlocksContent } from "@strapi/blocks-react-renderer";
+interface ProviderMetadata {
+  public_id: string;
+  resource_type: string;
+}
+export interface StrapiImage {
+  id: number;
+  documentId: string;
+  name: string;
+  alternativeText: string | null;
+  caption: string | null;
+  width: number;
+  height: number;
+  formats: Formats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: string | null;
+  provider: string;
+  provider_metadata: ProviderMetadata;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  locale: string | null;
+}
+
+interface Formats {
+  large: ImageFormat;
+  small: ImageFormat;
+  medium: ImageFormat;
+  thumbnail: ImageFormat;
+}
+
+interface ImageFormat {
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path: string | null;
+  size: number;
+  width: number;
+  height: number;
+  sizeInBytes: number;
+  provider_metadata: ProviderMetadata;
+}
+
 export type PropiedadType = {
   id: string;
   documentId: string;
@@ -15,6 +63,7 @@ export type PropiedadType = {
   bathrooms: number;
   land_surface: number;
   built_surface: number;
+  images: StrapiImage[];
 };
 
 type StrapiResponse<T> = {
@@ -30,7 +79,9 @@ type StrapiResponse<T> = {
 };
 
 export async function getListedProperties(): Promise<PropiedadType[]> {
-  const res = await fetch(`${import.meta.env.STRAPI_URL}/api/properties`);
+  const res = await fetch(
+    `${import.meta.env.STRAPI_URL}/api/properties&populate=images`
+  );
   const parsedResponse: StrapiResponse<PropiedadType[]> = await res.json();
   return parsedResponse.data;
 }
@@ -39,7 +90,7 @@ export async function getFeaturedProperties(): Promise<PropiedadType[]> {
   const res = await fetch(
     `${
       import.meta.env.STRAPI_URL
-    }/api/properties?filters[featured][$eq]=true&pagination[page]=1&pagination[pageSize]=3`
+    }/api/properties?filters[featured][$eq]=true&pagination[page]=1&pagination[pageSize]=3&populate=images`
   );
   const parsedResponse: StrapiResponse<PropiedadType[]> = await res.json();
   return parsedResponse.data;
@@ -48,7 +99,7 @@ export async function getPropertyByDocumentId(
   documentId: string
 ): Promise<PropiedadType> {
   const res = await fetch(
-    `${import.meta.env.STRAPI_URL}/api/properties/${documentId}`
+    `${import.meta.env.STRAPI_URL}/api/properties/${documentId}?populate=images`
   );
   const parsedResponse: StrapiResponse<PropiedadType> = await res.json();
   return parsedResponse.data;
