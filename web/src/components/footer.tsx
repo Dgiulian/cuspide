aabd1bbf5e9d4a5ff008fcb937e69394a9ff213d
Diego Giuliani <giuliani.diego@gmail.com>
2024-10-18 00:13:34 -0300

    add footer icons

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,39 +1,31 @@
 import React from "react";
+import FacebookIcon from "@/icons/facebook.svg";
+import InstagramIcon from "@/icons/instagram.svg";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t border-gray-300 dark:border-gray-800">
-      <p className="text-xs text-gray-900 dark:text-gray-400">
+    <footer className="flex gap-2 justify-between sm:flex-row py-6 w-full shrink-0 items-baseline px-4 md:px-6 border-t border-gray-300 dark:border-gray-800">
+      <p className="text-xs text-gray-900 dark:text-gray-400 ">
         &copy; 2024 Cuspide Bienes Raices. Todos los derechos reservados.
       </p>
-      <nav className="sm:ml-auto flex gap-4 sm:gap-6 justify-between">
-        <div className="flex flex-col align-middle justify-center space-y-2">
-          <p>Seguinos en</p>
+      <nav className="sm:ml-auto flex gap-4 sm:gap-6 items-center">
+        <div className="flex flex-col align-middle justify-center space-y-2 ">
+          <p className="text-sm">Seguinos en</p>
 
           <div className="flex justify-center space-x-2">
             <a href="https://www.facebook.com/CuspideBr">
-              {/* <Icon
-                name="facebook"
-                size="32"
-                className="bg-slate-200 p-2 text-gray-900 rounded-sm"
-              /> */}
+              <FacebookIcon width={24} height={24} />
             </a>
             <a href="https://www.instagram.com/cuspide.neuquen/">
-              {/* <Icon
-                name="instagram"
-                size="32"
-                className="bg-slate-200 p-2 text-gray-900 rounded-sm"
-              /> */}
+              <InstagramIcon width={24} height={24} />
             </a>
           </div>
         </div>
-        <a className="text-xs hover:text-gray-300" href="#">
-          {" "}
-          Términos de Servicio{" "}
+        <a className="text-sm hover:text-gray-300 inline-block" href="#">
+          Términos de Servicio
         </a>
-        <a className="text-xs hover:text-gray-300" href="#">
-          {" "}
-          Privacidad{" "}
+        <a className="text-sm hover:text-gray-300" href="#">
+          Privacidad
         </a>
       </nav>
     </footer>
